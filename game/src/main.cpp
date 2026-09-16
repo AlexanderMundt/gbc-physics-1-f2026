aef76790faa423d3102353d73f410609754fe9dd
AlexanderMundt <amundt123@gmail.com>
2026-09-16 18:11:01 -0400

    LE1 Done(?)

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -23,21 +23,20 @@ int main()
     //EX:
     //Note that since raylib is in RHS, positive rotations are CLOCKWISE
     //Negate the launch angle so that your launch direction vector points up
-    Vector2 exPos = { 400.0f, 400.0f };
-    Vector2 exDir = Vector2Rotate(Vector2UnitX, -30.0f * DEG2RAD);
+    //Vector2 exPos = { 400.0f, 400.0f };
+    //Vector2 exDir = Vector2Rotate(Vector2UnitX, -30.0f * DEG2RAD);
 
     while (!WindowShouldClose())
     {
         //Calculate the launchDir Vector2 by using the Vector2Rotate function
         //Be sure to convert launchAng from degrees to radians when assinging it to Vector2Rotate
-
+        launchDir = Vector2Rotate(Vector2UnitX, -launchAng * DEG2RAD);
+        
         //Calculate launchVel Vector2 by multiplying launchDir by launchSpd
+        launchVel = launchDir * launchSpd;
 
         BeginDrawing();
         ClearBackground(WHITE);
-
-        //Render launchVel as a line from launchPos to launchPos + launchVel
-        DrawCircleV(launchPos, 20.0f, GRAY);
 
         //GUI
         DrawText(TextFormat("Launch Angle %.1f", launchAng), 32.0f, 30.0f, 16, DARKGRAY);
@@ -45,9 +44,15 @@ int main()
         GuiSlider({ 20.0f, 50.0f, 160.0f, 40.0f }, "0", "90", &launchAng, 0.0f, 90.0f);
         GuiSlider({ 20.0f, 120.0f, 160.0f, 40.0f }, "10", "300", &launchSpd, 10.0f, 300.0f);
 
+        //Render launchVel as a line from launchPos to launchPos + launchVel
+        DrawCircleV(launchPos, 20.0f, GRAY);
+
+        //Red line
+        DrawLineEx(launchPos, launchPos + launchVel, 4.0f, RED);
+
         //EX:
         //An illistration of a rotated vector
-        DrawLineEx(exPos, exPos + exDir * 100.0f, 4.0f, ORANGE);
+        //DrawLineEx(exPos, exPos + exDir * 100.0f, 4.0f, ORANGE);
 
         EndDrawing();
     }
